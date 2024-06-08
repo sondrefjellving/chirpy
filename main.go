fd866e2c550e934f889e8db9f78d21ea5131deed
sondrefjellving <sondrefjellving@gmail.com>
2024-06-08 08:29:11 +0200

    add handlers that use file server

--- main.go
+++ main.go
@@ -2,17 +2,20 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"net/http"
 )
 
 func main() {
 	mux := http.NewServeMux()
 
+	mux.Handle("/", http.FileServer(http.Dir(".")))
+	mux.Handle("/assets", http.FileServer(http.Dir(".logo.png")))
 	server := &http.Server{
 		Addr:			":8080",
 		Handler:		mux,
 	}
 
 	fmt.Println("starting server...")
-	server.ListenAndServe()
+	log.Fatal(server.ListenAndServe())
 }
